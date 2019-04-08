3bf3591f3f7df1c965ff91bdb2f9bb61504f5260
McMakoff <38282022+McMakoff@users.noreply.github.com>
2019-04-08 18:35:28 +0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+jQuery(window).on('load',function(){
+
+	/*var population = $(".population-article__carousel"),
+		updates = $(".updates__carousel"),
+		rating = $(".rating__carousel"),
+		population_main = $('.population-article__carousel_main'),
+		category = $('.category__carousel'),
+		pharos_1200 = '(max-width: 1200px)',
+		pharos_1100 = '(max-width: 1100px)',
+		pharos_620 = '(max-width: 620px)',
+		pharos_600 = '(max-width: 1200px)',
+		ratingOptions = {
+		    loop:false,
+		    nav:true,
+		    items:1,
+		    responsiveClass:true,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        740:{
+		            items:2
+		        }
+		    }
+		},
+		
+		populationOptions = {
+		    loop:false,
+		    nav:true,
+		    items:1,
+		    responsiveClass:true,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        620:{
+		            items:2
+		        },
+		        900:{
+		            items:3
+		        }
+		    }
+		};
+
+	function responsive_slider (pharos, slider, options) {
+		if(window.matchMedia(pharos).matches){
+			slider.owlCarousel(options);
+		    slider.removeClass('d-flex');
+	} else {
+	       slider.trigger('destroy.owl.carousel');
+	       slider.addClass('d-flex');
+	    }
+
+	}
+
+	//responsive_slider(pharos_1200, population, populationOptions);
+	responsive_slider(pharos_1200, updates, populationOptions);
+	responsive_slider(pharos_1100, rating, ratingOptions);
+	responsive_slider(pharos_620, population_main, populationOptions);
+	responsive_slider(pharos_620, category, populationOptions);
+
+	$(window).on('resize', function(){
+		//responsive_slider(pharos_1200, population, populationOptions);
+		responsive_slider(pharos_1200, updates, populationOptions);
+		responsive_slider(pharos_1100, rating, ratingOptions);
+		responsive_slider(pharos_620, population_main, populationOptions);
+		responsive_slider(pharos_620, category, populationOptions);
+	})*/
+	
+});
